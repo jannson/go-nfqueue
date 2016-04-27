941eeabf1647033fdee7f5b4a00039a5faeac7e9
janson <gandancing@gmail.com>
2016-04-27 12:14:27 +0800

    setup update

--- examples/setup.sh
+++ examples/setup.sh
@@ -12,6 +12,8 @@ sudo iptables -I INPUT 1 -p icmp -j NFQUEUE --queue-num 0
 #show log
 cat /proc/net/netfilter/nf_queue
 
+#CONFIG_NETFILTER_NETLINK_QUEUE
+
 CC=arm-fw867-linux-uclibcgnueabi-gcc GOOS=linux GOARCH=arm GOARM=5 CGO_ENABLED=1 CGO_CFLAGS="-march=armv5" go build --ldflags '-s  -extldflags "-static"'
 
 cd /projects/workspace-go/src/github.com/OneOfOne/go-nfqueue/examples
